a8317675208b7dbbf0fa42ff2256386d1ef932c9
fuzzyTew <fuzzyTew@gmail.com>
2017-04-24 23:27:32 -0400

    Basical pusher signatures generated.

--- simple-experience/main.js
+++ simple-experience/main.js
@@ -9,29 +9,36 @@ window.onerror = function(msg, url, line) {
 };
 message('Loading ...')
 
+
 document.body.onload = function() {
 
+    var pusher = new Pusher('8999c2b708a9a3152cb7', {encrypted:true});
+    //var pusherWorldChannel = pusher.subscribe('private-world');
+
     var auth0lock = new Auth0Lock('yxH-gFdeeRbVBH1C7oYqnCIrzvmgUci0', 'fuzzytew.auth0.com', {
-            
+            languageDictionary:{title:'Simple Experience'},
+            theme:{/*logo:'',*/primaryColor:'#808080'},
+            auth:{scope:'openid pusherAuth',params:{socket:'3',channels:['private-world','private-extra']}}
         });
-    
-    auth0lock.on('authenticated', function(result) {
-        localStorage.setItem('accessToken', result.accessToken);
-        auth0.getUserInfo(result.accessToken, function(error, info) {
+        
+    function onAuthenticated() {
+        auth0lock.getUserInfo(localStorage.getItem('accessToken'), function(error, info) {
             if (error) {
                 auth0lock.show({flashMessage:{type:'error',text:error}});
                 return;
             }
             message(JSON.stringify(info));
         });
+    }
+    
+    auth0lock.on('authenticated', function(result) {
+        auth0lock.hide();
+        localStorage.setItem('accessToken', result.accessToken);
+        onAuthenticated();
     });
     
-    auth0lock.show({
-        flashMessage: {
-            type: 'success',
-            text: 'Simple Experience needs a way to identify you'
-        }
-    });
-   
-    
+    if (localStorage.getItem('accessToken'))
+        onAuthenticated();
+    else
+        auth0lock.show();
 };
