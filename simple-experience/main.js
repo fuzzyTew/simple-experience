8506feee47a0eab67f03705bd4a4c6408ecf2d29
fuzzyTew <fuzzyTew@gmail.com>
2017-04-24 23:17:30 -0400

    -

--- simple-experience/main.js
+++ simple-experience/main.js
@@ -1,23 +1,37 @@
-window.onerror = function(msg, url, line) {
-    alert(url + ':' + line + ': ');
-};
+function message(msg) {
+    var p = document.createElement('p');
+    p.appendChild(document.createTextNode(msg));
+    document.body.appendChild(p);
+}
 
-alert('hello!');
+window.onerror = function(msg, url, line) {
+    message(url + ':' + line + ': ');
+};
+message('Loading ...')
 
 document.body.onload = function() {
-
-    alert('loaded!');
 
     var auth0lock = new Auth0Lock('yxH-gFdeeRbVBH1C7oYqnCIrzvmgUci0', 'fuzzytew.auth0.com', {
             
         });
     
-    auth0lock.show({
-            flashMessage: {
-                type: 'success',
-                text: 'Simple Experience needs a way to identify you.'
+    auth0lock.on('authenticated', function(result) {
+        localStorage.setItem('accessToken', result.accessToken);
+        auth0.getUserInfo(result.accessToken, function(error, info) {
+            if (error) {
+                auth0lock.show({flashMessage:{type:'error',text:error}});
+                return;
             }
+            message(JSON.stringify(info));
         });
+    });
+    
+    auth0lock.show({
+        flashMessage: {
+            type: 'success',
+            text: 'Simple Experience needs a way to identify you'
+        }
+    });
    
     
 };
