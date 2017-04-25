921c39518cad5db88c3f5399d68b12710d2d773e
fuzzyTew <fuzzyTew@gmail.com>
2017-04-24 22:48:46 -0400

    Testing auth0 and simple js

--- simple-experience/main.js
+++ simple-experience/main.js
@@ -1,3 +1,37 @@
-document.body.onload = function() {
-    alert('Hello, world!');
+function message(msg) {
+    var p = document.createElement('p');
+    p.appendChild(document.createTextNode(msg));
+    document.body.appendChild(p);
 }
+
+window.onerror = function(msg, url, line) {
+    message(url + ':' + line + ': ');
+};
+message('Loading ...')
+
+document.body.onload = function() {
+
+    var auth0lock = new Auth0Lock('yxH-gFdeeRbVBH1C7oYqnCIrzvmgUci0', 'fuzzytew.auth0.com', {
+            
+        });
+    
+    auth0lock.on('authenticated', function(result) {
+        localStorage.setItem('accessToken', result.accessToken);
+        auth0.getUserInfo(result.accessToken, function(error, info) {
+            if (error) {
+                auth0lock.show({flashMessage:{type:'error',text:error}});
+                return;
+            }
+            message(JSON.stringify(info));
+        });
+    });
+    
+    auth0lock.show({
+        flashMessage: {
+            type: 'success',
+            text: 'Simple Experience needs a way to identify you'
+        }
+    });
+   
+    
+};
