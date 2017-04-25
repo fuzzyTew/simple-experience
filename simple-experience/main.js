cb050981871d3042d5b1b302bfc32da2d429dd10
fuzzyTew <fuzzyTew@gmail.com>
2017-04-24 23:27:32 -0400

    -

--- simple-experience/main.js
+++ simple-experience/main.js
@@ -9,29 +9,31 @@ window.onerror = function(msg, url, line) {
 };
 message('Loading ...')
 
+
 document.body.onload = function() {
 
     var auth0lock = new Auth0Lock('yxH-gFdeeRbVBH1C7oYqnCIrzvmgUci0', 'fuzzytew.auth0.com', {
             
         });
-    
-    auth0lock.on('authenticated', function(result) {
-        localStorage.setItem('accessToken', result.accessToken);
-        auth0.getUserInfo(result.accessToken, function(error, info) {
+        
+    function onAuthenticated() {
+        auth0lock.getUserInfo(localStorage.getItem('accessToken'), function(error, info) {
             if (error) {
                 auth0lock.show({flashMessage:{type:'error',text:error}});
                 return;
             }
             message(JSON.stringify(info));
         });
+    }
+    
+    auth0lock.on('authenticated', function(result) {
+        auth0lock.hide();
+        localStorage.setItem('accessToken', result.accessToken);
+        onAuthenticated();
     });
     
-    auth0lock.show({
-        flashMessage: {
-            type: 'success',
-            text: 'Simple Experience needs a way to identify you'
-        }
-    });
-   
-    
+    if (localStorage.getItem('authToken'))
+        onAuthenticated();
+    else
+        auth0lock.show({flashMessage: {type: 'success',text: 'Simple Experience needs a way to identify you'}});
 };
