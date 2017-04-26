b568290e74d54e0327b73cab1919aa5f89ed7328
fuzzyTew <fuzzyTew@gmail.com>
2017-04-26 13:45:38 -0400

    light grey background to detect overflow

--- simple-experience/gfx.js
+++ simple-experience/gfx.js
@@ -13,11 +13,10 @@ var gfx = {};
     gl.canvas.style.top = '0';
     gl.canvas.style.width = '100vw';
     gl.canvas.style.height = '100vh';
+    gl.canvas.style.zIndex = '-1';
     
     document.body.style.margin = 0;
     document.body.appendChild(gl.canvas);
-
-    var stillRender;
     
     window.onresize = function() {
         twgl.resizeCanvasToDisplaySize(gl.canvas);
@@ -25,8 +24,6 @@ var gfx = {};
 
         m4.perspective(Math.PI / 4, gl.canvas.clientWidth / gl.canvas.clientHeight, 0.0, 1024, scene.projection);
         scene.changed();
-        
-        //gfx.render();
     };
     
     
@@ -35,16 +32,17 @@ var gfx = {};
     gfx.scene = scene;
     
     gfx.testGL = function() {
+        gl.clearColor(0.9, 0.9, 0.9, 1.0);
         m4.lookAt([0,0,10], [0,0,0], [0,1,0], scene.camera);
         var obj1 = gfxgeom.Ellipsoid(scene, m4.multiply(m4.rotationZ(0.8), m4.scaling([2,1,1])));
         var obj2 = gfxgeom.Ellipsoid(scene, m4.translation([1.5,0,1.5]));
         function render(ms) {
+            gl.clear(gl.COLOR_BUFFER_BIT + gl.DEPTH_BUFFER_BIT);
             obj1.draw(gl);
             obj2.draw(gl);
             requestAnimationFrame(render);
         }
         window.onresize();
         requestAnimationFrame(render);
-        //stillRender = render;
     };
 })();
