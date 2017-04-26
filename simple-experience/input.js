b456904d1ee7b01e53256221a35e937c96580a16
fuzzyTew <fuzzyTew@gmail.com>
2017-04-26 14:39:15 -0400

    Preliminary code for simple touch events

--- simple-experience/input.js
+++ simple-experience/input.js
@@ -1,35 +1,88 @@
 var input = {};
 (function(){
-    var registerX, registerY;
-    var clicking;
-    window.addEventListener('mousedown', function(e) {
-        registerX = e.offsetX;
-        registerY = e.offsetY;
-        clicking = true;
-        return false;
-    });
-    window.addEventListener('mousemove', function(e) {
-        if (!e.buttons) return;
-        if (clicking) {
-            input.startdrag2d(registerX, registerY);
-            clicking = false;
-        }
-        input.ondrag2d(e.offsetX - registerX, e.offsetY - registerY);
-        registerX = e.offsetX;
-        registerY = e.offsetY;
-        return false;
-    });
-    window.addEventListener('mouseup', function(e) {
-        if (clicking) {
-            input.onclick2d(registerX, registerY);
-        } else {
-            input.stopdrag2d(e.offsetX, e.offsetY);
-        }
-        return false;
-    });
     
+    // ---- override these functions elsewhere to handle events
     input.onclick2d = function(x, y) {};
     input.startdrag2d = function(x, y) {};
     input.ondrag2d = function(dx, dy) {};
     input.stopdrag2d = function(x, y) {};
+    // ----
+
+
+    var registerX, registerY;
+    var clicking;
+
+    function down(x, y) {
+        registerX = x;
+        registerY = y;
+        clicking = true;
+    }
+
+    function move(x, y) {
+        if (clicking) {
+            input.startdrag2d(x, y);
+            clicking = false;
+        }
+        input.ondrag2d(x - registerX, y - registerY);
+        registerX = x;
+        registerY = y;
+    }
+
+    function up(x, y) {
+        if (clicking) {
+            input.onclick2d(registerX, registerY);
+        } else {
+            input.stopdrag2d(x, y);
+        }
+    }
+
+    window.addEventListener('mousedown', function(e) {
+        down(e.pageX, e.pageY);
+    });
+
+    window.addEventListener('mousemove', function(e) {
+        if (!e.buttons) return;
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        move(e.pageX, e.pageY);
+    });
+
+    window.addEventListener('mouseup', function(e) {
+        up(e.pageX, e.pageY);
+    });
+
+    window.addEventListener('touchstart', function(e) {
+        if (e.touches.length > 1)
+            return;
+
+        down(e.touches[0].pageX, e.touches[0].pageY);
+    });
+
+    window.addEventListener('touchmove', function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (e.touches.length > 1)
+            return;
+
+        move(e.touches[0].pageX, e.touches[0].pageY);
+    });
+
+    window.addEventListener('touchend', function(e) {
+        if (e.touches.length > 1)
+            return;
+
+        up(e.touches[0].pageX, e.touches[0].pageY);
+    });
+
+    window.addEventListener('touchcancel', function(e) {
+        if (e.touches.length > 1)
+            return;
+
+        up(e.touches[0].pageX, e.touches[0].pageY);
+    });
+
+
 })();
